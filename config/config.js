27c7288d1d4ea7c46e814d646e84355273ca91d9
Alexambau <alexambau@gmail.com>
2019-04-07 00:11:37 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 // The server port - the port to run Pokemon Showdown under h
 //exports.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 //exports.bindaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-exports.port = 8000;
+exports.port = 80;
 
 // proxyip - proxy IPs with trusted X-Forwarded-For headers
 //   This can be either false (meaning not to trust any proxies) or an array
